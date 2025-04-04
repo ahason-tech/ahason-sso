0cac282a9323afcda5f17b62a701d565deb01b81
ashok-murugan <ashok1murugan@gmail.com>
2025-04-05 03:28:11 +0530

    spring base

---
+++ src/main/java/com/ahason/sso/AhasOnSSO.java
@@ -0,0 +1,13 @@
+package com.ahason.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhasOnSSO {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhasOnSSO.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahason/sso/AhasOnSSOTests.java
@@ -0,0 +1,13 @@
+package com.ahason.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhasOnSSOTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
